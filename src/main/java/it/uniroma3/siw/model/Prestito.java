f54980caa56b0a1f714c88e9bc83f60164a67112
Federico Nardi <nardif2004@gmail.com>
2026-05-25 17:14:10 +0200

    Vincoli classe Prestito messi

--- src/main/java/it/uniroma3/siw/model/Prestito.java
+++ src/main/java/it/uniroma3/siw/model/Prestito.java
@@ -3,59 +3,69 @@ package it.uniroma3.siw.model;
 import java.time.LocalDate;
 import java.util.Objects;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.validation.constraints.NotNull;
 
 @Entity
 public class Prestito {
 
-@Id	
-@GeneratedValue(strategy = GenerationType.AUTO)	
-private Long id;
-private LocalDate startDate;
-private LocalDate endDate;
-private boolean returned;
-public Long getId() {
-	return id;
-}
-public void setId(Long id) {
-	this.id = id;
-}
-public LocalDate getStartDate() {
-	return startDate;
-}
-public void setStartDate(LocalDate startDate) {
-	this.startDate = startDate;
-}
-public LocalDate getEndDate() {
-	return endDate;
-}
-public void setEndDate(LocalDate endDate) {
-	this.endDate = endDate;
-}
-public boolean isReturned() {
-	return returned;
-}
-public void setReturned(boolean returned) {
-	this.returned = returned;
-}
-@Override
-public int hashCode() {
-	return Objects.hash(id);
-}
-@Override
-public boolean equals(Object obj) {
-	if (this == obj)
-		return true;
-	if (obj == null)
-		return false;
-	if (getClass() != obj.getClass())
-		return false;
-	Prestito other = (Prestito) obj;
-	return Objects.equals(id, other.id);
-}
+	@Id	
+	@GeneratedValue(strategy = GenerationType.AUTO)	
+	private Long id;
+	@NotNull
+	@Column(nullable = false)
+	private LocalDate startDate;
+	@NotNull
+	@Column(nullable = false)
+	private LocalDate endDate;
+	@Column(nullable = false)
+	private boolean returned;
+	
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public LocalDate getStartDate() {
+		return startDate;
+	}
+	public void setStartDate(LocalDate startDate) {
+		this.startDate = startDate;
+	}
+	public LocalDate getEndDate() {
+		return endDate;
+	}
+	public void setEndDate(LocalDate endDate) {
+		this.endDate = endDate;
+	}
+	public boolean isReturned() {
+		return returned;
+	}
+	public void setReturned(boolean returned) {
+		this.returned = returned;
+	}
+	
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+	
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Prestito other = (Prestito) obj;
+		return Objects.equals(id, other.id);
+	}
 
 
 }
