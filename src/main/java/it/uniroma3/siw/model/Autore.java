db0a47b944643387840bc8e52c188d9fec6dd726
Federico Nardi <nardif2004@gmail.com>
2026-05-25 14:30:22 +0200

    Vincoli classe Autore messi

--- src/main/java/it/uniroma3/siw/model/Autore.java
+++ src/main/java/it/uniroma3/siw/model/Autore.java
@@ -3,60 +3,67 @@ package it.uniroma3.siw.model;
 import java.time.LocalDate;
 import java.util.Objects;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.validation.constraints.NotBlank;
 
 @Entity
 public class Autore {
 
-@Id
-@GeneratedValue(strategy = GenerationType.AUTO)
-private Long id;
-private String name;
-private String surname;
-private LocalDate birthDate;
-public Long getId() {
-	return id;
-}
-public void setId(Long id) {
-	this.id = id;
-}
-public String getName() {
-	return name;
-}
-public void setName(String name) {
-	this.name = name;
-}
-public String getSurname() {
-	return surname;
-}
-public void setSurname(String surname) {
-	this.surname = surname;
-}
-public LocalDate getBirthDate() {
-	return birthDate;
-}
-public void setBirthDate(LocalDate birthDate) {
-	this.birthDate = birthDate;
-}
-@Override
-public int hashCode() {
-	return Objects.hash(name, surname);
-}
-@Override
-public boolean equals(Object obj) {
-	if (this == obj)
-		return true;
-	if (obj == null)
-		return false;
-	if (getClass() != obj.getClass())
-		return false;
-	Autore other = (Autore) obj;
-	return Objects.equals(name, other.name) && Objects.equals(surname, other.surname);
-}
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long id;
+	@NotBlank
+	@Column(nullable = false)
+	private String name;
+	@NotBlank
+	@Column(nullable = false)
+	private String surname;
 
+	private LocalDate birthDate;
+	
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getSurname() {
+		return surname;
+	}
+	public void setSurname(String surname) {
+		this.surname = surname;
+	}
+	public LocalDate getBirthDate() {
+		return birthDate;
+	}
+	public void setBirthDate(LocalDate birthDate) {
+		this.birthDate = birthDate;
+	}
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Autore other = (Autore) obj;
+		return Objects.equals(id, other.id);
+	}
 
 
 }
