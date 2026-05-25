de2a47c48cc3f5077a5a157a117eaa359d62025d
Federico Nardi <nardif2004@gmail.com>
2026-05-25 14:29:05 +0200

    Revert "Vincoli nel model aggiunti"
    This reverts commit 3a8dca62e1d7a42adfe3101ed06555a6c399e345.

--- src/main/java/it/uniroma3/siw/model/Recensione.java
+++ src/main/java/it/uniroma3/siw/model/Recensione.java
@@ -3,61 +3,52 @@ package it.uniroma3.siw.model;
 import java.time.LocalDate;
 import java.util.Objects;
 
-import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Size;
 
 @Entity
 public class Recensione {
 
-	@Id	
-	@GeneratedValue(strategy = GenerationType.AUTO)	
-	private Long id;
-	@NotBlank
-	@Size(min = 5, max = 1000)
-	@Column(nullable = false, length = 1000)
-	private String text;
-	@NotNull
-	@Column(nullable = false)
-	private LocalDate creationDate;
-	public Long getId() {
-		return id;
-	}
-	public void setId(Long id) {
-		this.id = id;
-	}
-	public String getText() {
-		return text;
-	}
-	public void setText(String text) {
-		this.text = text;
-	}
-	public LocalDate getCreationDate() {
-		return creationDate;
-	}
-	public void setCreationDate(LocalDate creationDate) {
-		this.creationDate = creationDate;
-	}
-	@Override
-	public int hashCode() {
-		return Objects.hash(id);
-	}
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		Recensione other = (Recensione) obj;
-		return Objects.equals(id, other.id);
-	}
+@Id	
+@GeneratedValue(strategy = GenerationType.AUTO)	
+private Long id;
+private String text;
+private LocalDate creationDate;
+public Long getId() {
+	return id;
+}
+public void setId(Long id) {
+	this.id = id;
+}
+public String getText() {
+	return text;
+}
+public void setText(String text) {
+	this.text = text;
+}
+public LocalDate getCreationDate() {
+	return creationDate;
+}
+public void setCreationDate(LocalDate creationDate) {
+	this.creationDate = creationDate;
+}
+@Override
+public int hashCode() {
+	return Objects.hash(id);
+}
+@Override
+public boolean equals(Object obj) {
+	if (this == obj)
+		return true;
+	if (obj == null)
+		return false;
+	if (getClass() != obj.getClass())
+		return false;
+	Recensione other = (Recensione) obj;
+	return Objects.equals(id, other.id);
+}
 
 
 }
--- src/main/java/it/uniroma3/siw/model/Utente.java
+++ src/main/java/it/uniroma3/siw/model/Utente.java
@@ -7,7 +7,6 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.validation.constraints.NotBlank;
 
 @Entity
 public class Utente {
@@ -15,13 +14,8 @@ public class Utente {
 	@Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
-	@NotBlank
-	@Column(nullable = false)
 	private String name;
-	@NotBlank
-	@Column(nullable = false)
 	private String surname;
-	@NotBlank
     @Column(nullable = false, unique = true)
 	private String email;
 
