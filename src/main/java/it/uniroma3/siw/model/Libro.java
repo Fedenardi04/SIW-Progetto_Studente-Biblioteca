f2c9811a604e7708eba7e15a045ff69ada513cf3
Federico Nardi <nardif2004@gmail.com>
2026-05-25 17:18:02 +0200

    Vincoli classe Libro messi

--- src/main/java/it/uniroma3/siw/model/Libro.java
+++ src/main/java/it/uniroma3/siw/model/Libro.java
@@ -2,60 +2,77 @@ package it.uniroma3.siw.model;
 
 import java.util.Objects;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 
 @Entity
 public class Libro {
 
-@Id
-@GeneratedValue(strategy = GenerationType.AUTO)
-private Long id;	
-private String title;
-private Integer year;
-private String urlImage;
-public Long getId() {
-	return id;
-}
-public void setId(Long id) {
-	this.id = id;
-}
-public String getTitle() {
-	return title;
-}
-public void setTitle(String title) {
-	this.title = title;
-}
-public Integer getYear() {
-	return year;
-}
-public void setYear(Integer year) {
-	this.year = year;
-}
-public String getUrlImage() {
-	return urlImage;
-}
-public void setUrlImage(String urlImage) {
-	this.urlImage = urlImage;
-}
-@Override
-public int hashCode() {
-	return Objects.hash(title, year);
-}
-@Override
-public boolean equals(Object obj) {
-	if (this == obj)
-		return true;
-	if (obj == null)
-		return false;
-	if (getClass() != obj.getClass())
-		return false;
-	Libro other = (Libro) obj;
-	return Objects.equals(title, other.title) && Objects.equals(year, other.year);
-}
-
-
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long id;	
+	@NotBlank
+	@Column(nullable = false)
+	private String title;
+	@NotNull
+	@Min(1450)
+	@Max(2026)
+	@Column(nullable = false)
+	private Integer year;
+	@Size(max = 2048)
+    @Pattern(regexp = "(https?://.*)?", message = "Must be a valid URL or empty")
+    @Column(nullable = true)
+	private String urlImage;
 	
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public Integer getYear() {
+		return year;
+	}
+	public void setYear(Integer year) {
+		this.year = year;
+	}
+	public String getUrlImage() {
+		return urlImage;
+	}
+	public void setUrlImage(String urlImage) {
+		this.urlImage = urlImage;
+	}
+	@Override
+	public int hashCode() {
+		return Objects.hash(title, year);
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Libro other = (Libro) obj;
+		return Objects.equals(title, other.title) && Objects.equals(year, other.year);
+	}
+
+
+
 }
