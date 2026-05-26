eebe6d54d3b0f9043e4d5d181c739045db65923b
Federico Nardi <nardif2004@gmail.com>
2026-05-26 12:46:47 +0200

    Cascade su Book-->reviews aggiunto

--- src/main/java/it/uniroma3/siw/model/Book.java
+++ src/main/java/it/uniroma3/siw/model/Book.java
@@ -3,6 +3,7 @@ package it.uniroma3.siw.model;
 import java.util.List;
 import java.util.Objects;
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
@@ -50,7 +51,7 @@ public class Book {
 	@OneToMany(mappedBy = "book")
 	private List<Loan> loans;
 	
-	@OneToMany(mappedBy = "book")
+	@OneToMany(mappedBy = "book", cascade = CascadeType.ALL)
 	private List<Review> reviews;
 	
 	public Long getId() {
