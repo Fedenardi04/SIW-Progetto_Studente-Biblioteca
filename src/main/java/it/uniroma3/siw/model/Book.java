2ab1ed5f908e35a44bd8065729e872130c1ecd33
Federico Nardi <nardif2004@gmail.com>
2026-05-26 11:38:44 +0200

    Vincolo unicità titolo/anno aggiunto alla classe Libro

--- src/main/java/it/uniroma3/siw/model/Book.java
+++ src/main/java/it/uniroma3/siw/model/Book.java
@@ -11,6 +11,8 @@ import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import jakarta.persistence.UniqueConstraint;
 import jakarta.validation.constraints.Max;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
@@ -19,6 +21,7 @@ import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
 
 @Entity
+@Table(uniqueConstraints = @UniqueConstraint(columnNames = {"title", "year"}))
 public class Book {
 
 	@Id
