72e1070663d56f43dc47ef3021165d96e2b3cff0
Federico Nardi <nardif2004@gmail.com>
2026-05-27 20:36:03 +0200

    Aggiunti template per Libro e HomePage

---
+++ src/main/resources/import.sql
@@ -0,0 +1,6 @@
+INSERT INTO book (id, title, year, url_image) VALUES
+(1, 'Dune', 1965, 'https://example.com/dune.jpg'),
+(2, '1984', 1949, 'https://example.com/1984.jpg'),
+(3, 'Il Signore degli Anelli', 1954, 'https://example.com/lotr.jpg'),
+(4, 'Harry Potter e la Pietra Filosofale', 1997, 'https://example.com/hp1.jpg'),
+(5, 'Neuromante', 1984, 'https://example.com/neuromante.jpg');
