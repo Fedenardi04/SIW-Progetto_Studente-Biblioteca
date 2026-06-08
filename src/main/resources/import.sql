0ea2c21b1a371611c3fff881c38e4b34d132f6bf
Federico Nardi <nardif2004@gmail.com>
2026-06-08 19:06:39 +0200

    Import.sql modificato

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,6 +1,14 @@
-INSERT INTO book (id, title, year, url_image) VALUES
-(1, 'Dune', 1965, 'https://example.com/dune.jpg'),
-(2, '1984', 1949, 'https://example.com/1984.jpg'),
-(3, 'Il Signore degli Anelli', 1954, 'https://example.com/lotr.jpg'),
-(4, 'Harry Potter e la Pietra Filosofale', 1997, 'https://example.com/hp1.jpg'),
-(5, 'Neuromante', 1984, 'https://example.com/neuromante.jpg');
+INSERT INTO book (id, title, year, url_image)
+VALUES (nextval('book_seq'), 'Dune', 1965, 'https://example.com/dune.jpg');
+
+INSERT INTO book (id, title, year, url_image)
+VALUES (nextval('book_seq'), '1984', 1949, 'https://example.com/1984.jpg');
+
+INSERT INTO book (id, title, year, url_image)
+VALUES (nextval('book_seq'), 'Il Signore degli Anelli', 1954, 'https://example.com/lotr.jpg');
+
+INSERT INTO book (id, title, year, url_image)
+VALUES (nextval('book_seq'), 'Harry Potter e la Pietra Filosofale', 1997, 'https://example.com/hp1.jpg');
+
+INSERT INTO book (id, title, year, url_image)
+VALUES (nextval('book_seq'), 'Neuromante', 1984, 'https://example.com/neuromante.jpg');
