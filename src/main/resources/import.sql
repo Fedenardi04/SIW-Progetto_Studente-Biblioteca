daa554bbbc076899dc372dcfef488d0b039e9b35
Federico Nardi <nardif2004@gmail.com>
2026-06-08 19:29:08 +0200

    Layer security tolto provvisoriamente

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,14 +1,5 @@
-INSERT INTO book (id, title, year, url_image)
-VALUES (nextval('book_seq'), 'Dune', 1965, 'https://example.com/dune.jpg');
-
-INSERT INTO book (id, title, year, url_image)
-VALUES (nextval('book_seq'), '1984', 1949, 'https://example.com/1984.jpg');
-
-INSERT INTO book (id, title, year, url_image)
-VALUES (nextval('book_seq'), 'Il Signore degli Anelli', 1954, 'https://example.com/lotr.jpg');
-
-INSERT INTO book (id, title, year, url_image)
-VALUES (nextval('book_seq'), 'Harry Potter e la Pietra Filosofale', 1997, 'https://example.com/hp1.jpg');
-
-INSERT INTO book (id, title, year, url_image)
-VALUES (nextval('book_seq'), 'Neuromante', 1984, 'https://example.com/neuromante.jpg');
+insert into book (id, title, year, url_image) values(nextval('book_seq'), 'Dune', 1965, 'https://example.com/dune.jpg');
+insert into book (id, title, year, url_image) values(nextval('book_seq'), '1984', 1949, 'https://example.com/1984.jpg');
+insert into book (id, title, year, url_image) values(nextval('book_seq'), 'Il Signore degli Anelli', 1954, 'https://example.com/lotr.jpg');
+insert into book (id, title, year, url_image) values(nextval('book_seq'), 'Harry Potter e la Pietra Filosofale', 1997, 'https://example.com/hp1.jpg');
+insert into book (id, title, year, url_image) values(nextval('book_seq'), 'Neuromante', 1984, 'https://example.com/neuromante.jpg');
