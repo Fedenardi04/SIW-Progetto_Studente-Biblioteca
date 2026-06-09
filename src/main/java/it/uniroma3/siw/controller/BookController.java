62a574939bedb3f5860708f69a800147aeb0d80d
Federico Nardi <nardif2004@gmail.com>
2026-06-09 19:07:41 +0200

    Form libro aggiunta, refactoring BookController

--- src/main/java/it/uniroma3/siw/controller/BookController.java
+++ src/main/java/it/uniroma3/siw/controller/BookController.java
@@ -5,8 +5,11 @@ import java.util.Optional;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 
+import it.siw.uniroma3.it.exception.DuplicateBookException;
 import it.uniroma3.siw.model.Book;
 import it.uniroma3.siw.service.BookService;
 
@@ -47,5 +50,36 @@ public class BookController {
 
 	    return "books/form";
 	}
+	
+	@PostMapping("/books")
+    public String saveBook(@ModelAttribute("book") Book book,
+                           Model model) {
+        try {
+            bookService.saveBook(book);
+            return "redirect:/books";
+        } catch (DuplicateBookException e) {
+            model.addAttribute("errorMessage", e.getMessage());
+            return "books/form";
+        }
+    }
+
+    @GetMapping("/books/edit/{id}")
+    public String editForm(@PathVariable Long id, Model model) {
+        Book book = bookService.findBookById(id)
+                .orElse(null);
+
+        if (book == null) {
+            return "redirect:/books";
+        }
+
+        model.addAttribute("book", book);
+        return "books/form";
+    }
+
+    @GetMapping("/books/delete/{id}")
+    public String deleteBook(@PathVariable Long id) {
+        bookService.deleteBook(id);
+        return "redirect:/books";
+    }
 	
 }
