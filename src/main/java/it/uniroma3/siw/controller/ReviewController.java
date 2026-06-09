c8ab812a5819b71adda50aa0a11fa3254f78be24
Federico Nardi <nardif2004@gmail.com>
2026-06-09 18:18:26 +0200

    ReviewController aggiunto

---
+++ src/main/java/it/uniroma3/siw/controller/ReviewController.java
@@ -0,0 +1,40 @@
+package it.uniroma3.siw.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import it.uniroma3.siw.model.Review;
+import it.uniroma3.siw.service.BookService;
+import it.uniroma3.siw.service.ReviewService;
+
+@Controller
+public class ReviewController {
+
+	private ReviewService reviewService;
+    private BookService bookService;
+
+	
+	public ReviewController(ReviewService reviewService, BookService bookService) {
+		super();
+		this.reviewService = reviewService;
+		this.bookService = bookService;
+	}
+
+	
+    @GetMapping("/books/{bookId}/reviews/new")
+    public String createForm(@PathVariable Long bookId, Model model) {
+
+        model.addAttribute("book", bookService.findBookById(bookId).get());
+        model.addAttribute("review", new Review());
+
+        return "reviews/form";
+    }
+
+    @PostMapping("/books/{bookId}/reviews")
+    public String save(@PathVariable Long bookId, @ModelAttribute Review review) {
+
+        reviewService.saveReview(bookId, review);
+        return "redirect:/books/" + bookId;
+    }
+}
