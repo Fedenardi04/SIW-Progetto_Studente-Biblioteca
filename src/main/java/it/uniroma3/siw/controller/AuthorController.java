3e654493afdd40c39f7ba6271d4289d24a1f26aa
Federico Nardi <nardif2004@gmail.com>
2026-07-10 11:53:04 +0200

    Refactoring BookController e AuthorController

--- src/main/java/it/uniroma3/siw/controller/AuthorController.java
+++ src/main/java/it/uniroma3/siw/controller/AuthorController.java
@@ -15,49 +15,91 @@ import it.uniroma3.siw.service.AuthorService;
 @Controller
 public class AuthorController {
 
-	private AuthorService authorService;
+    private final AuthorService authorService;
 
-	public AuthorController(AuthorService authorService) {
-		this.authorService = authorService;
-	}
+    public AuthorController(AuthorService authorService) {
+        this.authorService = authorService;
+    }
 
-	@GetMapping("/authors")
-	public String list(Model model) {
-	    model.addAttribute("authors", authorService.findAllAuthors());
-	    return "authors/list";
-	}
-	
-	@GetMapping("/authors/{id}")
-	public String show(@PathVariable Long id, Model model) {
+    /*
+     * PAGINE PUBBLICHE
+     */
 
-	    Optional<Author> author = authorService.findByIdWithBooks(id);
+    @GetMapping("/authors")
+    public String publicList(Model model) {
+        model.addAttribute("authors", authorService.findAllAuthors());
+        return "authors/list";
+    }
 
-	    if(author.isEmpty())
-	        return "redirect:/authors";
+    @GetMapping("/authors/{id}")
+    public String publicShow(@PathVariable Long id, Model model) {
 
-	    model.addAttribute("author", author.get());
+        Optional<Author> optionalAuthor =
+                authorService.findByIdWithBooks(id);
 
-	    return "authors/show";
-	}
-	
-	@GetMapping("/admin/authors/new")
-	public String createForm(Model model) {
-		
-		model.addAttribute("author", new Author());
-	    return "authors/form";
-	}
-	
-	@PostMapping("/admin/authors")
-	public String save(@ModelAttribute Author author,
-	                         Model model) {
+        if (optionalAuthor.isEmpty()) {
+            return "redirect:/authors";
+        }
 
-		authorService.saveAuthor(author);
-        return "redirect:/authors";
-	}
-	
-	@GetMapping("/authors/delete/{id}")
-	public String deleteAuthor(@PathVariable Long id) {
-	    authorService.deleteAuthor(id);
-	    return "redirect:/authors";
-	}
+        model.addAttribute("author", optionalAuthor.get());
+
+        return "authors/show";
+    }
+
+    /*
+     * PAGINE ADMIN
+     */
+
+    @GetMapping("/admin/authors")
+    public String adminList(Model model) {
+        model.addAttribute("authors", authorService.findAllAuthors());
+        return "admin/authors/list";
+    }
+    
+    @GetMapping("/admin/authors/{id}")
+    public String adminShow(@PathVariable Long id, Model model) {
+
+        Optional<Author> optionalAuthor =
+                authorService.findByIdWithBooks(id);
+
+        if (optionalAuthor.isEmpty()) {
+            return "redirect:/admin/authors";
+        }
+
+        model.addAttribute("author", optionalAuthor.get());
+
+        return "admin/authors/show";
+    }
+
+    @GetMapping("/admin/authors/new")
+    public String createForm(Model model) {
+        model.addAttribute("author", new Author());
+        return "admin/authors/form";
+    }
+
+    @PostMapping("/admin/authors")
+    public String save(@ModelAttribute("author") Author author) {
+        authorService.saveAuthor(author);
+        return "redirect:/admin/authors";
+    }
+
+    @GetMapping("/admin/authors/{id}/edit")
+    public String editForm(@PathVariable Long id, Model model) {
+
+        Author author = authorService.findAuthorById(id).orElse(null);
+
+        if (author == null) {
+            return "redirect:/admin/authors";
+        }
+
+        model.addAttribute("author", author);
+
+        return "admin/authors/form";
+    }
+
+    @PostMapping("/admin/authors/{id}/delete")
+    public String deleteAuthor(@PathVariable Long id) {
+        authorService.deleteAuthor(id);
+        return "redirect:/admin/authors";
+    }
 }
--- src/main/java/it/uniroma3/siw/controller/BookController.java
+++ src/main/java/it/uniroma3/siw/controller/BookController.java
@@ -14,75 +14,106 @@ import it.uniroma3.siw.service.AuthorService;
 import it.uniroma3.siw.service.BookService;
 import it.uniroma3.siw.service.LoanService;
 
-@Controller 
+@Controller
 public class BookController {
 
-	
+    private final BookService bookService;
+    private final AuthorService authorService;
+    private final LoanService loanService;
 
-	private BookService bookService;
-	private AuthorService authorService;
-	private LoanService loanService;
-
-	public BookController(BookService bookService, AuthorService authorService, LoanService loanService) {
-	    this.bookService = bookService;
-	    this.authorService = authorService;
-	    this.loanService = loanService;
-	}
-
-	
-	@GetMapping("/books")
-	public String list(Model model) {
-		model.addAttribute("books", bookService.findAllBooks());
-		return "books/list";
-	}
-	
-	@GetMapping("/books/{id}")
-	public String show(@PathVariable Long id, Model model) {
-
-	    Optional<Book> optional = bookService.findBookById(id);
-
-	    if (optional.isEmpty()) {
-	        return "redirect:/books";
-	    }
-
-	    model.addAttribute("book", optional.get());
-	    model.addAttribute("bookAlreadyLoaned", loanService.isBookCurrentlyLoaned(id));
-	    return "books/show";
-	}
-	
-	@GetMapping("/books/new")
-	public String createForm(Model model) {
-
-	    model.addAttribute("book", new Book());
-    model.addAttribute("authors", authorService.findAllAuthors());
-
-	    return "books/form";
-	}
-	
-	@PostMapping("/books")
-    public String saveBook(@ModelAttribute("book") Book book,
-                           Model model) {
-		bookService.saveBook(book);
-        return "redirect:/books";
+    public BookController(BookService bookService,
+                          AuthorService authorService,
+                          LoanService loanService) {
+        this.bookService = bookService;
+        this.authorService = authorService;
+        this.loanService = loanService;
     }
 
-    @GetMapping("/books/edit/{id}")
+    /*
+     * PAGINE PUBBLICHE
+     */
+
+    @GetMapping("/books")
+    public String publicList(Model model) {
+        model.addAttribute("books", bookService.findAllBooks());
+        return "books/list";
+    }
+
+    @GetMapping("/books/{id}")
+    public String publicShow(@PathVariable Long id, Model model) {
+
+        Optional<Book> optionalBook = bookService.findBookById(id);
+
+        if (optionalBook.isEmpty()) {
+            return "redirect:/books";
+        }
+
+        model.addAttribute("book", optionalBook.get());
+        model.addAttribute(
+                "bookAlreadyLoaned",
+                loanService.isBookCurrentlyLoaned(id)
+        );
+
+        return "books/show";
+    }
+
+    /*
+     * PAGINE ADMIN
+     */
+
+    @GetMapping("/admin/books")
+    public String adminList(Model model) {
+        model.addAttribute("books", bookService.findAllBooks());
+        return "admin/books/list";
+    }
+    
+    @GetMapping("/admin/books/{id}")
+    public String adminShow(@PathVariable Long id, Model model) {
+
+        Optional<Book> optionalBook = bookService.findBookById(id);
+
+        if (optionalBook.isEmpty()) {
+            return "redirect:/admin/books";
+        }
+
+        model.addAttribute("book", optionalBook.get());
+        model.addAttribute("bookAlreadyLoaned", loanService.isBookCurrentlyLoaned(id));
+
+        return "admin/books/show";
+    }
+
+    @GetMapping("/admin/books/new")
+    public String createForm(Model model) {
+        model.addAttribute("book", new Book());
+        model.addAttribute("authors", authorService.findAllAuthors());
+
+        return "admin/books/form";
+    }
+
+    @PostMapping("/admin/books")
+    public String saveBook(@ModelAttribute("book") Book book) {
+        bookService.saveBook(book);
+        return "redirect:/admin/books";
+    }
+
+    @GetMapping("/admin/books/{id}/edit")
     public String editForm(@PathVariable Long id, Model model) {
+
         Book book = bookService.findBookById(id).orElse(null);
 
         if (book == null) {
-            return "redirect:/books";
+            return "redirect:/admin/books";
         }
 
         model.addAttribute("book", book);
         model.addAttribute("authors", authorService.findAllAuthors());
-        return "books/form";
+
+        return "admin/books/form";
     }
 
-    @GetMapping("/books/delete/{id}")
+    @PostMapping("/admin/books/{id}/delete")
     public String deleteBook(@PathVariable Long id) {
         bookService.deleteBook(id);
-        return "redirect:/books";
+        return "redirect:/admin/books";
     }
-	
 }
